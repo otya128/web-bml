a4572e7690af81888cf25b40152aaa1bb175d66c
otya <otya128@users.noreply.github.com>
2022-04-16 21:15:41 +0900

    受信機内蔵音をキャッシュするように
    12kHzだとちょっと音が変になるので24kHzに

--- client/romsound.ts
+++ client/romsound.ts
@@ -18,8 +18,10 @@
  * 15:
 **/
 
-function playBuffer(context: AudioContext, buf: Float32Array) {
-    const buffer = context.createBuffer(1, buf.length, context.sampleRate)
+const sampleRate = 12000 * 2;
+
+function playBuffer(context: AudioContext, buf: Float32Array, sampleRate: number) {
+    const buffer = context.createBuffer(1, buf.length, sampleRate)
     buffer.copyToChannel(buf, 0)
     const source = context.createBufferSource();
     source.buffer = buffer;
@@ -32,74 +34,85 @@ function sine(sampleRate: number, i: number, freq: number) {
     return Math.sin(i / (sampleFreq / (Math.PI * 2)));
 }
 
+const romSoundCache = new Map<number, Float32Array>();
 export function playRomSound(soundId: number, context: AudioContext) {
-    switch (soundId) {
-        case 5:
-            playSound5(context);
-            break;
-        case 7:
-            playSound7(context);
-            break;
-        case 9:
-            playSound9(context);
-            break;
-        default:
-            break;
+    let cache = romSoundCache.get(soundId);
+    if (cache == null) {
+        switch (soundId) {
+            case 5:
+                cache = generateSound5(sampleRate);
+                break;
+            case 7:
+                cache = generateSound7(sampleRate);
+                break;
+            case 9:
+                cache = generateSound9(sampleRate);
+                break;
+            default:
+                break;
+        }
+        if (cache != null) {
+            romSoundCache.set(soundId, cache);
+        }
+    }
+    if (cache != null) {
+        playBuffer(context, cache, sampleRate);
+        return;
     }
 }
 
 // 選択音
-function playSound5(context: AudioContext) {
-    const buf = new Float32Array(context.sampleRate * 0.2), volume = 0.1;
-    const len = context.sampleRate * 0.2;
+function generateSound5(sampleRate: number): Float32Array {
+    const buf = new Float32Array(sampleRate * 0.2), volume = 0.1;
+    const len = sampleRate * 0.2;
     for (let i = 0; i < len; i++) {
-        buf[i] += sine(context.sampleRate, i, 4680) * volume * (1 - i / len); // envelope
+        buf[i] += sine(sampleRate, i, 4680) * volume * (1 - i / len); // envelope
     }
     for (let i = 0; i < len; i++) {
-        buf[i] += sine(context.sampleRate, i, 4160) * volume * (1 - i / len);
+        buf[i] += sine(sampleRate, i, 4160) * volume * (1 - i / len);
     }
     for (let i = 0; i < len; i++) {
-        buf[i] += sine(context.sampleRate, i, 3640) * volume * 0.25 * (1 - i / len);
+        buf[i] += sine(sampleRate, i, 3640) * volume * 0.25 * (1 - i / len);
     }
     for (let i = 0; i < len; i++) {
-        buf[i] += sine(context.sampleRate, i, 520) * volume * (1 - i / len);
+        buf[i] += sine(sampleRate, i, 520) * volume * (1 - i / len);
     }
-    playBuffer(context, buf);
+    return buf;
 }
 
 // 6は5の連続
 
 // 決定音
-function playSound7(context: AudioContext) {
-    const buf = new Float32Array(context.sampleRate * 0.2), volume = 0.1;
-    for (let i = 0; i < context.sampleRate * 0.05; i++) {
-        buf[i] += sine(context.sampleRate, i, 2100) * volume;
+function generateSound7(sampleRate: number): Float32Array {
+    const buf = new Float32Array(sampleRate * 0.2), volume = 0.1;
+    for (let i = 0; i < sampleRate * 0.05; i++) {
+        buf[i] += sine(sampleRate, i, 2100) * volume;
     }
-    for (let i = context.sampleRate * 0.04; i < context.sampleRate * 0.2; i++) {
-        buf[i] += sine(context.sampleRate, i, 1400) * volume;
+    for (let i = sampleRate * 0.04; i < sampleRate * 0.2; i++) {
+        buf[i] += sine(sampleRate, i, 1400) * volume;
     }
-    for (let i = context.sampleRate * 0.04; i < context.sampleRate * 0.2; i++) {
-        buf[i] += sine(context.sampleRate, i, 4200) * volume * 0.6;
+    for (let i = sampleRate * 0.04; i < sampleRate * 0.2; i++) {
+        buf[i] += sine(sampleRate, i, 4200) * volume * 0.6;
     }
-    playBuffer(context, buf);
+    return buf;
 }
 
 // 8は7の連続
 
 // 選択音
-function playSound9(context: AudioContext) {
-    const buf = new Float32Array(context.sampleRate * 0.09), volume = 0.1;
-    const len = context.sampleRate * 0.09;
+function generateSound9(sampleRate: number): Float32Array {
+    const buf = new Float32Array(sampleRate * 0.09), volume = 0.1;
+    const len = sampleRate * 0.09;
     for (let i = 0; i < len; i++) {
-        buf[i] += sine(context.sampleRate, i, 5200) * volume * (1 - i / len); // envelope
+        buf[i] += sine(sampleRate, i, 5200) * volume * (1 - i / len); // envelope
     }
     for (let i = 0; i < len; i++) {
-        buf[i] += sine(context.sampleRate, i, 3120) * volume * (1 - i / len);
+        buf[i] += sine(sampleRate, i, 3120) * volume * (1 - i / len);
     }
     for (let i = 0; i < len; i++) {
-        buf[i] += sine(context.sampleRate, i, 1040) * volume * (1 - i / len);
+        buf[i] += sine(sampleRate, i, 1040) * volume * (1 - i / len);
     }
-    playBuffer(context, buf);
+    return buf;
 }
 
 // 10は9の連続
